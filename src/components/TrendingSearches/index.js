bacea53c33ce2f8736fcaf1b365ff7bbb39a0b09
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-10 15:32:54 -0500

    Lazy Load

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,12 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import getTrendingTerms from '../../services/getTrendingTermsService';
 import Category from '../Category';
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([]);
 
   useEffect(() => {
     getTrendingTerms().then(setTrends)
   }, []);
   return <Category name='Tendencias' options={trends} />
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+  const refLazyLoad = useRef();
+
+  useEffect(function () {
+    const onChange = (entries, observer) => {
+      const el = entries[0];
+      console.log(el.isIntersecting)
+      if (el.isIntersecting) {
+        setShow(true);
+        // esto es en caso de que solo quiera escuchar solo un intersect y nada mas, lo que hacemos es desconectar el intersect
+        // con la funcion disconnect()
+        observer.disconnect();
+      }
+    }
+    console.log(refLazyLoad.current)
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: '100px'
+    })
+    observer.observe(refLazyLoad.current)
+
+    return () => observer.disconnect()
+  })
+  return <div ref={refLazyLoad}>
+    {show ? <TrendingSearches /> : null}
+  </div>
 }
